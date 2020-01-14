677c2a6c027eb69981d5876b56df784fa6bb5eb7
unknown <RocKa840@ltpemdomain.local>
2020-01-14 14:15:32 +0100

    commit

---
+++ EXCERISE ONE/exc-1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function onClick(x) {
+    if (document.getElementById("PLUS").classList.contains())
+        (document.getElementById("MIN").classList.contains())
+}
+
+function reset() {
+
+}
